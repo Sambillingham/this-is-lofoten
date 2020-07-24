cc1f5aa14220ec9f28ba154dcea9510834769780
Sam Billingham <sambillie.sb@gmail.com>
2020-07-24 18:04:24 +0200

    fix: (layout) size

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,6 +5,10 @@
 </template>
 
 <style>
+html, body {
+  margin: 0;
+  padding: 0;
+}
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
