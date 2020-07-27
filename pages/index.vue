ff9573ac2c9032e9614461becd3527aac49efa34
Sam Billingham <sambillie.sb@gmail.com>
2020-07-27 15:39:09 +0200

    add: (index) facebook sharer

--- pages/index.vue
+++ pages/index.vue
@@ -106,10 +106,33 @@
         <a
           target="_blank"
           v-if="drawerOculusLink"
-          class="w-50 block bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full text-center"
+          class="w-50 block bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full text-center mb-2"
           :href="drawerOculusLink"
         >
           Save To Oculus
+        </a>
+        <a
+          :href="
+            `https://www.facebook.com/sharer/sharer.php?u=https://thisislofoten.com/location/${drawerVideoID}`
+          "
+          onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=800,width=500');return false;"
+          target="_blank"
+          title="Share on Facebook"
+          class="w-50 block text-white font-bold py-2 px-4 rounded-full text-center fb-btn mb-2"
+        >
+          <svg
+            height="24px"
+            width="24px"
+            role="img"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Facebook icon</title>
+            <path
+              d="M23.9981 11.9991C23.9981 5.37216 18.626 0 11.9991 0C5.37216 0 0 5.37216 0 11.9991C0 17.9882 4.38789 22.9522 10.1242 23.8524V15.4676H7.07758V11.9991H10.1242V9.35553C10.1242 6.34826 11.9156 4.68714 14.6564 4.68714C15.9692 4.68714 17.3424 4.92149 17.3424 4.92149V7.87439H15.8294C14.3388 7.87439 13.8739 8.79933 13.8739 9.74824V11.9991H17.2018L16.6698 15.4676H13.8739V23.8524C19.6103 22.9522 23.9981 17.9882 23.9981 11.9991Z"
+            />
+          </svg>
+          Share on Facebook
         </a>
         <span @click="closeDrawer()" class="close cursor-pointer">&times;</span>
       </div>
@@ -900,5 +923,18 @@ export default {
 .intro .btn:hover {
   background: #152965;
   box-shadow: 0 5px 10px rgba(50, 50, 50, 0.2);
+}
+.fb-btn {
+  background: #1877f2;
+  cursor: pointer;
+  position: relative;
+}
+
+.fb-btn svg {
+  fill: #fff;
+  position: absolute;
+  left: 0.75rem;
+  top: 50%;
+  transform: translateY(-50%);
 }
 </style>
