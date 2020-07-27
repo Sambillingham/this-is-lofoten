932e01a0530822814ebb14325dcd7e2c57241358
Sam Billingham <sambillie.sb@gmail.com>
2020-07-27 12:55:20 +0200

    add: (location) meta tags

--- pages/location/_location.vue
+++ pages/location/_location.vue
@@ -34,9 +34,30 @@ export default {
       meta: [
         // hid is used as unique identifier. Do not use `vmid` for it as it will not work
         {
-          hid: this.vid,
-          name: this.content.title,
+          hid: 'description',
+          name: 'description',
           content: this.content.description,
+        },
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: this.content.title,
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: this.content.description,
+        },
+        {
+          hid: 'og:url',
+          property: 'og:url',
+          content: this.BASE_URL + this.$route.path,
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content:
+            this.BASE_URL + require(`~/assets/img/${this.content.description}`),
         },
       ],
     }
