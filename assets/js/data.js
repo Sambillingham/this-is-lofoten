81ea41bca48a5158bc00e4b90cfb0413373e18ba
Sam Billingham <sambillie.sb@gmail.com>
2020-11-06 16:14:29 +0100

    update: (data) – add new video IDs

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,14 +1,14 @@
 export const videoData = [
-  {
-    coordinates: [14.387833, 68.232056],
-    title: 'Calm day in snowy mountains Lofoten Småtindan',
-    short_title: 'Småtindan Winter',
-    description:
-      'Relax with this beautiful winter landscape from Småtindan, Kabelvåg, Lofoten.',
-    thumbnail: 'Smatindan-360-VR.jpg',
-    videoID: 'V4TEhrYG168',
-    categories: ['Vågan', 'Relaxing', 'Winter'],
-  },
+  // {
+  //   coordinates: [14.387833, 68.232056],
+  //   title: 'Calm day in snowy mountains Lofoten Småtindan',
+  //   short_title: 'Småtindan Winter',
+  //   description:
+  //     'Relax with this beautiful winter landscape from Småtindan, Kabelvåg, Lofoten.',
+  //   thumbnail: 'Smatindan-360-VR.jpg',
+  //   videoID: 'V4TEhrYG168',
+  //   categories: ['Vågan', 'Relaxing', 'Winter'],
+  // },
   {
     coordinates: [14.5875426, 68.2462474],
     title: 'Svolværgeita in 360',
@@ -16,7 +16,7 @@ export const videoData = [
     description:
       'Experience how it is to stand on top of the Svolvær goat in this 360 VR video! Warning!!! Not for people with fear of heights!!!',
     thumbnail: 'Svolværgeita-360-VR.jpg',
-    videoID: 'cFvkC9WzMtI',
+    videoID: 'K1G8o13XiG0',
     categories: ['Vågan', 'Summer', 'Activites'],
   },
   {
@@ -26,7 +26,7 @@ export const videoData = [
     description:
       'Fly over the amazing Kongstind Mountain, 552 meters above sea level. Looking down at Svolvær.',
     thumbnail: 'Kongstind-360-VR.jpg',
-    videoID: 'jXKmHpAQMdM',
+    videoID: 'O745j11TMJs',
     categories: ['Vågan', 'Winter', 'Aerials'],
   },
   {
@@ -36,7 +36,7 @@ export const videoData = [
     description:
       'See the amazing view from Himmeltinden, Vestvågøy. Looking down at Uttakleiv and the famous Haukland beach. This is springtime in Lofoten, with two different seasons at the same time! Winter in the mountains and (almost) summer at sea level.',
     thumbnail: 'Himmeltind-360-VR.jpg',
-    videoID: 'dBWjef-2nn8',
+    videoID: 'P9dHkQ7R3Y0',
     categories: ['Vestvågøy', 'Winter', 'Aerials'],
   },
   {
@@ -46,7 +46,7 @@ export const videoData = [
     description:
       'Join us on a flight over Lofoten! We are flying a Cessna F172P from Svolvær Airport, Helle. Thanks to pilot Alexander Jansen, and Lofoten Flyklubb for this amazing experience!',
     thumbnail: 'Cessna-flight-over-Lofoten-360-VR.jpg',
-    videoID: 'eDx-F-RBdc8',
+    videoID: 'zY_ZD_xxWtU',
     categories: ['Vestvågøy', 'Spring', 'Aerials'],
   },
   {
@@ -56,7 +56,7 @@ export const videoData = [
     description:
       'Enjoy this famous view from Reinebringen in June, when the sun never goes down. Looking down at Reine, Sakrisøy, Hamnøy and into Bunesfjorden and Kjerkfjorden (videos from these places are coming to the plattform soon). Check out www.catogarden.no for a unique place to stay and activities from the area.',
     thumbnail: 'Reinebringen-360-VR.jpg',
-    videoID: 'SMU8ObOUw0k',
+    videoID: 'VcOOfUzPQPM',
     categories: ['Moskenes', 'Summer', 'Aerials'],
     oculus:
       'https://www.oculus.com/experiences/media/291491408930952/3499066823459640/',
@@ -68,7 +68,7 @@ export const videoData = [
     description:
       'See the Skrova Island from above. Skrova is a small island group and village area in Vågan. The 2.5-square-kilometre Skrova island group in the Lofoten archipelago, it is located in the Vestfjorden, south of the islands of Vågana, Lillemolla, and Stormolla. The largest island in the group is Storskrova and the island of Heimskrova is where most of the population is located. The six main inhabited islands in the group are all interconnected by a small bridge and several man-made causeways.',
     thumbnail: 'skrova.jpg',
-    videoID: 'kBVvGXquN5o',
+    videoID: '9F4wdgUPER4',
     categories: ['Vågan', 'Spring', 'Aerials'],
   },
   {
@@ -78,29 +78,29 @@ export const videoData = [
     description:
       'Enjoy this popular viewpoint from the air. Here you can see Himmeltinden behind Borge in the front and Eggum to the right.',
     thumbnail: 'Torvdalen.jpg',
-    videoID: 'CfyMVOzK9aU',
+    videoID: 'b5igaFdqb7M',
     categories: ['Vestvågøy', 'Spring', 'Aerials'],
   },
-  {
-    coordinates: [14.72544, 68.253901],
-    title: 'Eagle Safari - Risvær',
-    short_title: 'Eagle Safari',
-    description:
-      'XXLofoten offers RIB tours from Svolvær. <br/><br/> There is a large population of Sea eagles in Raftsundet, and one of the highlights is without doubt when a sea eagle circle a few meters above your head. <br/><br/> This is the eagle with the largest wing span in the world, up to 2,65 meters. Our guides have a high awareness on safety, and will make sure that you have a comfortable experience. Therefore, this tour is a good option for the whole family. <br/> <a class="font-bold" target="_blank" href="https://www.xxlofoten.no/zodiac-safari/">https://www.xxlofoten.no/zodiac-safari/</a>',
-    thumbnail: 'Eagle-safari-360-VR.jpg',
-    videoID: 'xtV0vW_e4wg',
-    categories: ['Vågan', 'Spring', 'Activites'],
-  },
-  {
-    coordinates: [14.681032, 68.323608],
-    title: 'Vestpollen aerial',
-    short_title: 'Vestpollen',
-    description:
-      'Enjoy this flight over the calm waters in Vestpollen with beautiful evening light.',
-    thumbnail: 'Eagle-safari-360-VR.jpg',
-    videoID: 'zJaMeS1Lt5Y',
-    categories: ['Vågan', 'Spring', 'Aerials'],
-  },
+  // {
+  //   coordinates: [14.72544, 68.253901],
+  //   title: 'Eagle Safari - Risvær',
+  //   short_title: 'Eagle Safari',
+  //   description:
+  //     'XXLofoten offers RIB tours from Svolvær. <br/><br/> There is a large population of Sea eagles in Raftsundet, and one of the highlights is without doubt when a sea eagle circle a few meters above your head. <br/><br/> This is the eagle with the largest wing span in the world, up to 2,65 meters. Our guides have a high awareness on safety, and will make sure that you have a comfortable experience. Therefore, this tour is a good option for the whole family. <br/> <a class="font-bold" target="_blank" href="https://www.xxlofoten.no/zodiac-safari/">https://www.xxlofoten.no/zodiac-safari/</a>',
+  //   thumbnail: 'Eagle-safari-360-VR.jpg',
+  //   videoID: 'xtV0vW_e4wg',
+  //   categories: ['Vågan', 'Spring', 'Activites'],
+  // },
+  // {
+  //   coordinates: [14.681032, 68.323608],
+  //   title: 'Vestpollen aerial',
+  //   short_title: 'Vestpollen',
+  //   description:
+  //     'Enjoy this flight over the calm waters in Vestpollen with beautiful evening light.',
+  //   thumbnail: 'Eagle-safari-360-VR.jpg',
+  //   videoID: 'zJaMeS1Lt5Y',
+  //   categories: ['Vågan', 'Spring', 'Aerials'],
+  // },
   {
     coordinates: [13.54661, 68.082691],
     title: 'Ballstad Aerial',
@@ -108,7 +108,7 @@ export const videoData = [
     description:
       'See Ballstad in Lofotenfrom above! This is from the spring, when all the cod is being dried on the racks in almost all the towns in Lofoten.',
     thumbnail: 'Ballstad.jpg',
-    videoID: 'QtgAM7cpaeM',
+    videoID: 'rb0UjVXfvFg',
     categories: ['Vestvågøy', 'Spring', 'Aerials'],
   },
   {
@@ -118,7 +118,7 @@ export const videoData = [
     description:
       'Look around on this popular photo spot. Looking up at Stjerntinden.',
     thumbnail: 'RoadNusfjord.jpg',
-    videoID: 'uajMZaVYDAg',
+    videoID: '9LQM3YjIGtg',
     categories: ['Flakstadøy', 'Winter', 'Relaxing'],
   },
   {
@@ -128,7 +128,7 @@ export const videoData = [
     description:
       'Look around on this popular photo spot. Looking down at Storvatnet and up at Stjerntinden.',
     thumbnail: 'storvatnet.jpg',
-    videoID: 'c2nU_Cr03Dg',
+    videoID: '6SB7Wu_lvy8',
     categories: ['Flakstadøy', 'Winter', 'Relaxing'],
   },
   {
@@ -138,7 +138,7 @@ export const videoData = [
     description:
       '<p>Experience the famous Trollfjord! You will be rewarded with breathtaking & magnificent views of steep mountains dropping straight into the fjord. The 2-kilometre (1.2 mi) long fjord cuts into the island of Vågana and flows out into the Raftsundet strait. The fjord has a narrow entrance and steep-sided mountains surrounding it. The name is derived from troll, a figure from Norse mythology.</p><br /> <p>Enjoy this magic hybrid electric cruise with BRIM EXPLORER: <a class="font-bold" target="_blank" href="https://brimexplorer.com/tours/silent"> https://brimexplorer.com/tours/silent.</a> </p>',
     thumbnail: 'trollfjorden.jpg',
-    videoID: 'zZHoOmhafRA',
+    videoID: 'ybfU6pO1HX0',
     categories: ['Vågan', 'Summer', 'Activites', 'Aerials'],
   },
   {
@@ -147,7 +147,7 @@ export const videoData = [
     short_title: 'Paragliding Gimsøy',
     description: 'Fly out off the Storknubben mountain in a paraglider!',
     thumbnail: 'paragliding-gimsøy.jpg',
-    videoID: 'yEZxd_Az8Oo',
+    videoID: 'YwzomXEcVxo',
     categories: ['Vågan', 'Summer', 'Activites', 'Aerials'],
   },
   {
@@ -157,7 +157,7 @@ export const videoData = [
     description:
       'Enjoy this classic photo spot in Reine. Please remember that this is private property if you ever want to go here. So please ask for permission before entering!',
     thumbnail: 'reine.jpg',
-    videoID: 'JEKi9D93qk0',
+    videoID: 'FxmhpQ0V0hA',
     categories: ['Moskenes', 'Winter', 'Relaxing'],
   },
   {
@@ -167,7 +167,7 @@ export const videoData = [
     description:
       'Rock climbing	Experience this Classic climbing route in Kalle, Lofoten. Its called Rom og Cola, and goes up the Småkallan Mountain (418 meters above sea level). Warning! Not for people with fear of heights!',
     thumbnail: 'climb-lofo.jpg',
-    videoID: 'T_0h3KY1x_4',
+    videoID: 'B6-A2KrN7lY',
     categories: ['Austvågøy', 'Summer', 'Activites'],
   },
   {
@@ -177,7 +177,7 @@ export const videoData = [
     description:
       'This place is actually called Paradise, because it is! Relax your mind while enjoying the view towards Vågakallen.',
     thumbnail: 'paradise.jpg',
-    videoID: 'ZHu3-qfLK9Y',
+    videoID: '8lt1CTa9IpQ',
     categories: ['Austvågøy', 'Summer', 'Relaxing'],
   },
   {
@@ -187,19 +187,19 @@ export const videoData = [
     description:
       'Fly over the harbour and breakwater in Kabelvåg. Looking down at the clear ocean.',
     thumbnail: 'kabelvag-harbour.jpg',
-    videoID: '8O4lmQlpCis',
+    videoID: 'rTdezEudgyE',
     categories: ['Austvågøy', 'Summer', 'Aerial'],
   },
-  {
-    coordinates: [13.105326, 67.941087],
-    title: 'Kayak paddling in Reine',
-    short_title: 'Kayak in Reine',
-    description:
-      'Enjoy the scenery in Reine with Reine Paddling (https://www.facebook.com/reinepaddling). This must be one of the nicest place in the world to go kayaking!',
-    thumbnail: 'kajakk.jpg',
-    videoID: '7CCl7fzL7wA',
-    categories: ['Moskenes', 'Summer', 'Aerial', 'Activities'],
-  },
+  // {
+  //   coordinates: [13.105326, 67.941087],
+  //   title: 'Kayak paddling in Reine',
+  //   short_title: 'Kayak in Reine',
+  //   description:
+  //     'Enjoy the scenery in Reine with Reine Paddling (https://www.facebook.com/reinepaddling). This must be one of the nicest place in the world to go kayaking!',
+  //   thumbnail: 'kajakk.jpg',
+  //   videoID: '7CCl7fzL7wA',
+  //   categories: ['Moskenes', 'Summer', 'Aerial', 'Activities'],
+  // },
   {
     coordinates: [13.522697, 68.210069],
     title: 'Uttakleiv Aerial',
@@ -207,7 +207,7 @@ export const videoData = [
     description:
       'Explore this popular bay, Uttakleiv on Vestvågøy in Lofoten from the air!',
     thumbnail: 'utakleiv.jpg',
-    videoID: 'oPPLh27ZLBg',
+    videoID: '2DjPL5p9S6s',
     categories: ['Vestvågøy', 'Summer', 'Aerial'],
   },
   {
@@ -216,7 +216,7 @@ export const videoData = [
     short_title: 'Magic summer night',
     description: 'Fly over the calm waters in Alstad, Lofoten.',
     thumbnail: 'alstad.jpg',
-    videoID: 'WhGc_Ko6A_0',
+    videoID: 'UkruUguJDsA',
     categories: ['Vestvågøy', 'Summer', 'Aerial'],
   },
   {
@@ -226,7 +226,7 @@ export const videoData = [
     description:
       'Fly over the Fredvang bridges close to Ramberg with the pointy Volandstinden in front.',
     thumbnail: 'fredvang-bridges.jpg',
-    videoID: 'WdKBBmRMhrw',
+    videoID: 'PZsaYNrD2pY',
     categories: ['Moskenes', 'Flakstadøy', 'Summer', 'Aerial'],
   },
   {
@@ -236,7 +236,7 @@ export const videoData = [
     description:
       'Feel the stoke from the snowboarders and skiers at the Kongstind Alpine center!',
     thumbnail: 'snowboarding-kongstind.jpg',
-    videoID: 'R-n1P3QTvpw',
+    videoID: 'xwn6-NJNDEw',
     categories: ['Vågan', 'Winter', 'Activities'],
   },
   {
@@ -246,7 +246,7 @@ export const videoData = [
     description:
       'Experience the view from the sea level in Reine. Looking up on Reinebringen',
     thumbnail: 'reine-winter.jpg',
-    videoID: '4OiD_RcFKqA',
+    videoID: 'MEUwJBL1-zI',
     categories: ['Moskenes', 'Winter', 'Relaxing'],
   },
   {
@@ -256,7 +256,7 @@ export const videoData = [
     description:
       'Get a close (!) view of the cod heads on the drying racks in Reine.',
     thumbnail: 'cod-drying-hamnøy.jpg',
-    videoID: 'ip1ZyeB1Img',
+    videoID: 'VcOZWyi6i7c',
     categories: ['Moskenes', 'Winter', 'Relaxing'],
   },
   {
@@ -266,7 +266,7 @@ export const videoData = [
     description:
       'Look out on the harbour in Hamnøy and towards the majestic Olstinden with fishing boats in front.',
     thumbnail: 'hamnøy-harbour.jpg',
-    videoID: 'r7IloYPqTSk',
+    videoID: 'dJzBIkqMl5I',
     categories: ['Moskenes', 'Winter', 'Relaxing'],
   },
   {
@@ -276,7 +276,7 @@ export const videoData = [
     description:
       'See the fog dancing over Hopspollen with Vågakallen in the background.',
     thumbnail: 'hopspollen-fog.jpg',
-    videoID: 'OKxxS2FhwkM',
+    videoID: '2tu3ORZtMdQ',
     categories: ['Vågan', 'Summer', 'Relaxing'],
   },
   {
@@ -286,7 +286,7 @@ export const videoData = [
     description:
       'Fly up close to Nonstinden on Litlmolla, with the green ocean underneath you. Looking down on the RIB from XXLofoten (https://www.xxlofoten.no/rib-safaris/)',
     thumbnail: 'litlmolla.jpg',
-    videoID: 'BBd9CBQTi94',
+    videoID: 'NsaiwUoiNz8',
     categories: ['Vågan', 'Spring', 'Aerial'],
   },
   {
@@ -296,7 +296,7 @@ export const videoData = [
     description:
       'Relax with the surroundings of Nordpollen, the calm water and the sunset over Laukvika.',
     thumbnail: 'nordpollen.jpg',
-    videoID: 'VxCA7L0d5qs',
+    videoID: 'S-lQb-ccugU',
     categories: ['Vågan', 'Spring', 'Aerial'],
   },
   {
@@ -306,7 +306,7 @@ export const videoData = [
     description:
       'Watch the cod racks in Reine with Kirkfjorden in the background.',
     thumbnail: 'reine-cod.jpg',
-    videoID: 'pPOV51SrbuU',
+    videoID: 'PQQKnsbIQeU',
     categories: ['Moskenes', 'Winter', 'Relaxing'],
   },
   {
@@ -315,7 +315,7 @@ export const videoData = [
     short_title: 'Storvatnet Winter',
     description: 'Fly over Storvatnet covered with snow and ice.',
     thumbnail: 'storvatnet-aerial.jpg',
-    videoID: 'DihOILJBB9o',
+    videoID: 'nsKuvlaP1DY',
     categories: ['Vågan', 'Winter', 'Aerials'],
   },
 ]
